db6eb574fe5cd70613b5a62137aec7f1e6c129ed
Vadimchik79 <vadim.komarov.2000.mm@mail.ru>
2020-02-19 01:57:01 +0500

    Добавлены подключения библиотек.

--- DXE_Start/Program.cs
+++ DXE_Start/Program.cs
@@ -3,6 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SharpDX;
+using SharpDX.Animation;
+using SharpDX.D3DCompiler;
+using SharpDX.Diagnostics;
+using SharpDX.Direct3D;
+using SharpDX.Direct3D12;
+using SharpDX.DirectComposition;
+using SharpDX.DirectInput;
+using SharpDX.DirectManipulation;
+using SharpDX.DirectSound;
+using SharpDX.DirectWrite;
+using SharpDX.DXGI;
+using SharpDX.IO;
+using SharpDX.Mathematics;
+using SharpDX.Multimedia;
+using SharpDX.RawInput;
+using SharpDX.Text;
+using SharpDX.WIC;
+using SharpDX.Win32;
+using SharpDX.Windows;
+using SharpDX.X3DAudio;
+using SharpDX.XAPO;
+using SharpDX.XAudio2;
 
 namespace DXE_Start
 {
